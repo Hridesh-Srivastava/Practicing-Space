102f04298765260aac2dfd5cd2946b5ed3eeeee9
Hridesh Srivastava <143743755+Hridesh-Srivastava@users.noreply.github.com>
2024-08-31 20:12:48 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/' , (_ , res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit" , (req  , res) => {
+    const lengthTotal = req.body["first"].length + req.body["last"].length;
+
+    res.render("index.ejs" , {
+        key1 : lengthTotal,
+    });
+});
+
+app.listen(port , () => {
+    console.log(`listening on port ${port}`);
+});
